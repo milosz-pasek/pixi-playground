efbcc01764c254ef3acc459a18ea3dafc4356300
Miłosz Pasek <mpasek@agh.edu.pl>
2025-01-03 13:49:28 +0100

    test keyboard movement

--- src/main.ts
+++ src/main.ts
@@ -15,21 +15,40 @@ import { Application, Assets, Sprite } from "pixi.js";
 
   // Create a bunny Sprite
   const bunny = new Sprite(texture);
-
+  bunny.on("pointerdown", (e) => {
+    console.log("Bunny clicked!");
+  });
+  bunny.eventMode = "static";
   // Center the sprite's anchor point
   bunny.anchor.set(0.5);
-
   // Move the sprite to the center of the screen
   bunny.position.set(app.screen.width / 2, app.screen.height / 2);
 
   // Add the bunny to the stage
   app.stage.addChild(bunny);
 
+  document.addEventListener("keydown", (e) => {
+    const speed = 5;
+    switch (e.key) {
+      case "ArrowUp":
+        bunny.y -= speed;
+        break;
+      case "ArrowDown":
+        bunny.y += speed;
+        break;
+      case "ArrowLeft":
+        bunny.x -= speed;
+        break;
+      case "ArrowRight":
+        bunny.x += speed;
+        break;
+    }
+  });
   // Listen for animate update
   app.ticker.add((time) => {
     // Just for fun, let's rotate mr rabbit a little.
     // * Delta is 1 if running at 100% performance *
     // * Creates frame-independent transformation *
-    bunny.rotation += 0.1 * time.deltaTime;
+    bunny.rotation += 0.05 * time.deltaTime;
   });
 })();
